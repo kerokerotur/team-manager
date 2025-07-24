21966300717fa334e387f2d2e6d64e2b7d6fbf5d
Kinaru Honma <naruball.ex@gmail.com>
2025-07-24 22:13:01 +0900

    日本日付の利用

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,13 @@ import 'package:flutter_template/src/router.dart';
 import 'package:flutter_template/src/widgets/app_footer.dart';
 import 'package:flutter_template/src/widgets/hamburger_menu.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:lottie/lottie.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ja_JP');
   await LocalNotificationService.initialize();
   await Firebase.initializeApp(
       // options: DefaultFirebaseOptions.currentPlatform,
